3559c0bdda7aabcffd3fe52428ee2dda5170afc3
dltndus <red77black@gmail.com>
2021-03-15 20:56:48 +0900

    add modify delete

---
+++ src/main/java/com/sc/it/VO/UserVO.java
@@ -0,0 +1,10 @@
+package com.sc.it.VO;
+
+public class UserVO {
+	private String s_name;
+	private String s_id;
+	private String s_pw;
+	private String s_gender;
+	private String s_email;
+	private String s_phone;
+}
