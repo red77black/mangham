e81f59a60fd5f00f4bb5e378062fd4fcb9d0579c
akfls367 <akfls367@gmail.com>
2021-03-16 17:07:35 +0900

    add modify delete

---
+++ src/main/java/com/sc/it/dao/SuSooUserDAO.java
@@ -0,0 +1,8 @@
+package com.sc.it.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class SuSooUserDAO {
+
+}
---
+++ src/main/java/com/sc/it/dao/SuSooUserMapper.java
@@ -0,0 +1,5 @@
+package com.sc.it.dao;
+
+public interface SuSooUserMapper {
+
+}
